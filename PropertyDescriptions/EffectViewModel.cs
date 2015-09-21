ddb7e3e82405094307f60f37dd317ab686d0dd8c
Igor Ralic <iralic@outlook.com>
2015-09-21 18:10:21 +0200

    Initial commit

---
+++ PropertyDescriptions/EffectViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Lumia.Imaging;
+using Lumia.Imaging.Adjustments;
+using System;
+
+namespace PropertyDescriptions
+{
+public class EffectViewModel
+{
+    public BlurEffect blur { get; set; }
+
+    public EffectViewModel()
+    {
+        this.blur = new BlurEffect();
+
+        DetectPropertyDescriptions();
+    }
+
+    private void DetectPropertyDescriptions()
+    {
+        IPropertyDescriptions blurPropertyDescriptions = this.blur as IPropertyDescriptions;
+
+        if (blurPropertyDescriptions != null)
+        {
+            var propertyDescription = blurPropertyDescriptions.PropertyDescriptions[nameof(this.blur.KernelSize)];
+
+            this.DefaultValue = (int) propertyDescription.DefaultValue;
+            this.MinValue = (int) propertyDescription.MinValue;
+            this.MaxValue = (int) propertyDescription.MaxValue;
+        }
+    }
+
+    public int DefaultValue { get; set; }
+    public int MaxValue { get; set; }
+    public int MinValue { get; set; }
+    }
+}
---
+++ PropertyDescriptions/MainPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Lumia.Imaging;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PropertyDescriptions
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private BitmapImage bitmap;
+        private WriteableBitmap resultBitmap;
+        private EffectViewModel viewModel;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.viewModel = new EffectViewModel();
+        }
+
+        private async void LoadButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            await PickImageAsync();
+        }
+
+        private async Task PickImageAsync()
+        {
+            var filePicker = new FileOpenPicker
+            {
+                SuggestedStartLocation = PickerLocationId.PicturesLibrary,
+                ViewMode = PickerViewMode.Thumbnail
+            };
+
+            filePicker.FileTypeFilter.Clear();
+            filePicker.FileTypeFilter.Add(".jpeg");
+            filePicker.FileTypeFilter.Add(".jpg");
+            filePicker.FileTypeFilter.Add(".png");
+
+            StorageFile file = await filePicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read);
+                this.bitmap = new BitmapImage();
+                this.bitmap.SetSource(fileStream);
+
+                this.resultBitmap = new WriteableBitmap(bitmap.PixelWidth, bitmap.PixelHeight);
+
+                this.LoadedImage.Source = this.bitmap;
+
+                fileStream.Seek(0);
+
+                this.viewModel.blur.Source = new RandomAccessStreamImageSource(fileStream);
+            }
+        }
+
+        private void DefaultButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.EffectRangeSlider.Value = this.viewModel.DefaultValue;
+        }
+
+        private async void EffectRangeSlider_OnValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            // only if bitmap is loaded
+            if (this.bitmap != null)
+            {
+                this.viewModel.blur.KernelSize = (int)e.NewValue;
+
+                using (var renderer = new WriteableBitmapRenderer(this.viewModel.blur, this.resultBitmap))
+                {
+                    await renderer.RenderAsync();
+                }
+
+                this.resultBitmap.Invalidate();
+
+                this.LoadedImage.Source = this.resultBitmap;
+            }
+        }
+    }
+}
